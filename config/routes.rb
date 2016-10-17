1ce26224c68912aa122c215267f680b856de127a
david <areq22@aliyun.com>
2016-10-17 18:49:10 +0800

    improve my account

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
-  resources :users
+  resources :users do
+    get 'account', on: :collection
+  end
+
+
+
+  
+
 end
