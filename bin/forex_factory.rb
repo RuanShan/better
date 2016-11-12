b3cc4840ec845989672ab27bd590cf869c4492b9
david <areq22@aliyun.com>
2016-11-12 09:46:03 +0800

    add more admin sidebar

--- bin/forex_factory.rb
+++
@@ -1,8 +0,0 @@
-require "em-eventsource"
-EM.run do
-  source = EventMachine::EventSource.new("http://example.com/streaming")
-  source.message do |message|
-    puts "new message #{message}"
-  end
-  source.start # Start listening
-end
