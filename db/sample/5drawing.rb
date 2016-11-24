895112087b09c4c5571e80c6cbdcb5958696ffc5
david <areq22@aliyun.com>
2016-11-24 11:39:52 +0800

    disable 5drawing.rb, it cause error when load sample

--- db/sample/5drawing.rb
+++ db/sample/5drawing.rb
@@ -1,2 +1,3 @@
-
-FactoryGirl.create(:drawing, user_bank: @user_bank, amount: rand(100)+50).process
+# it cause
+# ActiveRecord::RecordInvalid: 验证失败: Amountamount greater than balance
+# FactoryGirl.create(:drawing, user_bank: @user_bank, amount: rand(100)+50).process
