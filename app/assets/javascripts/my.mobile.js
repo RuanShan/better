aaf5fff4d5089a976ac75e29117f15b0966722bb
david <areq22@aliyun.com>
2016-11-26 18:07:06 +0800

    mobile support wip

--- app/assets/javascripts/my.mobile.js
+++ app/assets/javascripts/my.mobile.js
@@ -2,3 +2,25 @@
 //= require jquery.easing.min
 //= require jquery.simplesidebar
 //= require user.mobile
+
+$( document ).ready(function() {
+	$( '.sidebar' ).simpleSidebar({
+		settings: {
+			opener: '#open-sbl',
+			wrapper: '.wrapper',
+			animation: {
+				duration: 500,
+				easing: 'easeOutQuint'
+			}
+		},
+		sidebar: {
+			align: 'left',
+			width: 200,
+			closingLinks: 'a',
+		}
+	});
+  $(".subNav").click(function(){
+  			// 修改数字控制速度， slideUp(500)控制卷起速度
+  			$(this).next(".navContent").slideToggle(500);
+  });
+});
