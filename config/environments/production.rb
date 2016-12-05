0832b2c6cf5c5c03a9630f5e4c1a645b1ec4ada6
david <areq22@aliyun.com>
2016-12-05 11:57:40 +0800

    user '/private' as relative_url_root

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.relative_url_root = '/r'
+  config.relative_url_root = '/private'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
