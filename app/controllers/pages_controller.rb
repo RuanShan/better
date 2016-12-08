01c254e4e7d2f0d730cf1c24db238268bfbcaae7
david <areq22@aliyun.com>
2016-12-08 14:07:43 +0800

    improve forex_simple

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
   layout :layout_for_page
-  before_action :set_cros_header
+  #before_action :set_cros_header
+  before_action :set_symbol_by_params
   helper_method :current_seller
 
   def current_seller
@@ -17,6 +18,13 @@ class PagesController < ApplicationController
     else
       'application'
     end
+  end
+
+  def set_symbol_by_params
+    if Forex.symbols.include? params[:symbol]
+      @symbol = params[:symbol]
+    end
+    @symbol ||= Forex.symbols.first
   end
 
   def set_cros_header
