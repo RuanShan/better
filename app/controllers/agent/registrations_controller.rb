154029e573812680560df3724244bb49fef3aff5
david <areq22@aliyun.com>
2016-12-13 15:24:59 +0800

    fix borker sign up layout

--- app/controllers/agent/registrations_controller.rb
+++ app/controllers/agent/registrations_controller.rb
@@ -1,4 +1,6 @@
-class Agent::RegistrationsController < Devise::RegistrationsController
+class Agent::RegistrationsController < DeviseInvitable::RegistrationsController
+  layout "agent"
+
 # before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
 
