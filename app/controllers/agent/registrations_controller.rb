094fc2205983111e0ff6e58f777b490233fadc76
hui-green <zhangqh1984@sina.com>
2016-12-16 22:54:12 +0800

    fix agent signup bug

--- app/controllers/agent/registrations_controller.rb
+++ app/controllers/agent/registrations_controller.rb
@@ -10,9 +10,28 @@ class Agent::RegistrationsController < DeviseInvitable::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+   def create
+     build_resource(sign_up_params)
+
+     resource.save
+     yield resource if block_given?
+     if resource.persisted?
+       if resource.active_for_authentication?
+         #set_flash_message! :notice, :signed_up
+         sign_up(resource_name, resource)
+         redirect_to agent_broker_index_path
+         #respond_with resource, location: after_sign_up_path_for(resource)
+       else
+         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+         expire_data_after_sign_in!
+         respond_with resource, location: after_inactive_sign_up_path_for(resource)
+       end
+     else
+       clean_up_passwords resource
+       set_minimum_password_length
+       respond_with resource
+     end
+   end
 
   # GET /resource/edit
   # def edit
@@ -25,9 +44,13 @@ class Agent::RegistrationsController < DeviseInvitable::RegistrationsController
   # end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+    resource.destroy
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    #set_flash_message! :notice, :destroyed
+    yield resource if block_given?
+    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
