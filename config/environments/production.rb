625eae6ad92704cc4ca31e1a697ed54a7ab13773
david <areq22@aliyun.com>
2016-12-26 09:48:38 +0800

    fix stmp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,13 +70,14 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: Rails.application.secrets.domain_name,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    address: '103.214.35.119',
+    port: 465,
+    domain: 'ballmerasian.com',
+    user_name: 'admin',
+    password: ENV['BETTER_EMAIL_PWD'],
+    authentication: 'login',
+    openssl_verify_mode: 'none',
+    enable_starttls_auto: false
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
