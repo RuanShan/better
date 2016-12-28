23ff068b046f7cdb77ad4a41a427d9a1ceac9dc5
david <areq22@aliyun.com>
2016-12-28 16:05:18 +0800

    correct default_rate

--- db/migrate/20161226251704_change_rate_to_integer.rb
+++ db/migrate/20161226251704_change_rate_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeRateToInteger < ActiveRecord::Migration[5.0]
   def change
-    add_column :game_instruments, :default_rate, :decimal, null: false, default: 0.70 # 70%
+    add_column :game_instruments, :default_rate, :decimal, scale: 2, precision: 6,  null: false, default: 0.70 # 70%
   end
 end
