eaa475374e636e4b82311572bddf4784c74c50f7
david <areq22@aliyun.com>
2017-01-12 20:29:36 +0800

    add method user_life.recompute！

--- app/models/user_life.rb
+++ app/models/user_life.rb
@@ -6,6 +6,22 @@ class UserLife < ApplicationRecord
   belongs_to :user
   belongs_to :broker, optional: true
 
+  #
+  def recompute!
+    self.deposit_amount = user.deposits.with_state(:success).sum(:amount)
+    self.drawing_amount = user.drawings.with_state(:success).sum(:amount)
+    self.bid_amount = user.bids.sum(:amount)
+    bonuses = user.wallets.bonuses.includes(:originator)
+    self.bonus = bonuses.select{|bonus| bonus.originator.is_a? Deposit }.sum(&:amount)
+
+    # 赢了多前
+    self.profit =bonuses.select{|bonus| bonus.originator.is_a? Bid }.sum(&:amount)
+
+    self.balance = user.wallets.sum(:amount)
+
+    self.save!
+  end
+
   #可用现金
   def net_cash
     balance > delta_cash ? delta_cash : 0;
