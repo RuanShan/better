d5e9e5ab5bb7872a0578ca4f0008e2afc2cf46f4
david <areq22@aliyun.com>
2017-01-15 22:15:04 +0800

    fix my/* view sidebar

--- app/assets/javascripts/my.mobile.js
+++ app/assets/javascripts/my.mobile.js
@@ -44,21 +44,21 @@ $( document ).ready(function() {
   });
 
 
-	//$( '.sidebar' ).simpleSidebar({
-	//	settings: {
-	//		opener: '#open-sbl',
-	//		wrapper: '.wrapper',
-	//		animation: {
-	//			duration: 500,
-	//			easing: 'easeOutQuint'
-	//		}
-	//	},
-	//	sidebar: {
-	//		align: 'left',
-	//		width: 200,
-	//		closingLinks: 'a',
-	//	}
-	//});
+	$( '.sidebar' ).simpleSidebar({
+		settings: {
+			opener: '#open-sbl',
+			wrapper: '.wrapper',
+			animation: {
+				duration: 500,
+				easing: 'easeOutQuint'
+			}
+		},
+		sidebar: {
+			align: 'left',
+			width: 200,
+			closingLinks: 'a',
+		}
+	});
 	$(".subNav").click(function(){
   			// 修改数字控制速度， slideUp(500)控制卷起速度
   			$(this).next(".navContent").slideToggle(500);
